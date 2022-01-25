0e946e92120a51c5dbd964e30c3ef9824d6f66bd
Victor Caruso <vic.caruso@gmail.com>
2022-01-25 15:47:13 -0800

    okay learned how to run tests in terminal and now can confirm test is passing

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header/Header';
 
 test('renders the header', () => {
-  render(<Header />);
-  const linkElement = screen.getByText(/Alchemy Animal Farm/i);
+  render(<Header text='Alchemy Animal Farm'/>);
+  const linkElement = screen.getByText('Alchemy Animal Farm');
   expect(linkElement).toBeInTheDocument();
 });
