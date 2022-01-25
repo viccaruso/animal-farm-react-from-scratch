4a52952427ef69914e794da9ab345f7a96416b3e
Victor Caruso <vic.caruso@gmail.com>
2022-01-25 15:43:16 -0800

    made a test for header so the test passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders the header', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Alchemy Animal Farm/i);
   expect(linkElement).toBeInTheDocument();
 });
