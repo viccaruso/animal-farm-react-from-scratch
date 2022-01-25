52a6075e4d43055a201a7a0a2b17d6cb3c903036
Victor Caruso <vic.caruso@gmail.com>
2022-01-24 18:10:38 -0800

    Finished file setup and got background image to apply to Main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Main from './Main/Main';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import { animals } from './data';
 
 function App() {
   return (
-    <h1>Hello there!</h1>
+    <Main></Main>
   );
 }
 
