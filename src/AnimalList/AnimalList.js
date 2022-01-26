2a00d76b0b63cc745abc3c6e8e87517ae589001a
Victor Caruso <vic.caruso@gmail.com>
2022-01-25 17:04:26 -0800

    set up AnimalList component

--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -1,1 +1,13 @@
+import React from 'react';
 import Animal from '../Animal/Animal';
+import './AnimalList.css';
+
+export default function AnimalList({ animals }) {
+  return (
+    <div>
+      {
+        animals.map((animal, i) => <Animal key={`${animal.name + i}`} animal={animal} />)
+      }
+    </div>
+  );
+}
