5d26a6ef51fbeafed4e71687f494de163d881c78
Victor Caruso <vic.caruso@gmail.com>
2022-01-25 16:14:18 -0800

    updated prop name in test to use correct name of 'greeting'

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header/Header';
 
 test('renders the header', () => {
-  render(<Header text='Alchemy Animal Farm'/>);
+  render(<Header greeting='Alchemy Animal Farm'/>);
   const linkElement = screen.getByText('Alchemy Animal Farm');
   expect(linkElement).toBeInTheDocument();
 });
