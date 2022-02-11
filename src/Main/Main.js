54188e5923d9626d503ce26fa151bf10efd45c9d
Victor Caruso <vic.caruso@gmail.com>
2022-02-10 16:23:22 -0800

    fixed Main component class refactor

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import './Main.css';
 import AnimalList from '../AnimalList/AnimalList';
-const backgroundImg = '../background.png';
 
-export default function Main({ animals }) {
-  return (
-    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      <AnimalList animals={animals} />
-    </main>
-  );
-}
-
-
-
-// export default class Main extends Component {
-//   render() {
-//     return (
-//       <main>
-//         <AnimalList animals={this.props.animals} />
-//       </main>
-//     );
-//   }
+// export default function Main({ animals }) {
+//   return (
+//     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+//       <AnimalList animals={animals} />
+//     </main>
+//   );
 // }
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main>
+        <AnimalList animals={this.props.animals} />
+      </main>
+    );
+  }
+}
